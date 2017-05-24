eb3038a3e2e5522f056f66028ae8b76e4d60cb24
Luke Rollans <hello@lukerollans.me>
2017-05-24 18:13:09 +0800

    Implement FizzBuzz

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -1,4 +1,21 @@
 defmodule FizzBuzz do
-  def fizzer(number) do
+  def fizzer(number) when is_number(number) and rem(number, 3) === 0  and rem(number, 5) === 0 do
+    "FizzBuzz"
+  end
+
+  def fizzer(number) when is_number(number) and rem(number, 3) === 0 do
+    "Fizz"
+  end
+
+  def fizzer(number) when is_number(number) and rem(number, 5) === 0 do
+    "Buzz"
+  end
+
+  def fizzer(number) when is_number(number) do
+    number
+  end
+
+  def fizzer(_) do
+    raise "Not a number"
   end
 end
